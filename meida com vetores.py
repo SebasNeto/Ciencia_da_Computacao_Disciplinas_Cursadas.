5c92cb3b924e428aa90f9c74f28759d73df8a53c
Sebastião Bicharra Neto <97459779+SebasNeto@users.noreply.github.com>
2023-02-21 13:38:23 -0300

    Delete meida com vetores.py

--- meida com vetores.py
+++
@@ -1,9 +0,0 @@
-VetA = eval(input("Alunos: "))
-VetB = eval(input("nota disciplina B: "))
-VetC = eval(input("notas disciplina C: "))
-VetD = []
-for aluno in range(len(VetA)):
-    VetD.append((VetB[aluno]+VetC[aluno])/2)
-for aluno in range(len(VetA)):
-    print("Aluno: ",VetA[aluno],"-","media: ",VetD[aluno])
-    
