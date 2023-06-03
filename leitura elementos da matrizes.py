604588af400bd03c214b6a225b9edd045a21be39
Sebastião Bicharra Neto <97459779+SebasNeto@users.noreply.github.com>
2023-06-03 00:00:43 -0300

    Delete leitura elementos da matrizes.py

--- leitura elementos da matrizes.py
+++
@@ -1,5 +0,0 @@
-matriz = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-for linha in range(0,6):
-    for coluna in range(0,5):
-        matriz[linha][coluna] = int(input([linha,coluna]))
-print(matriz)
