d4c8b0fa7037cb422a2422b73ed9f0eb46ea5e15
Sebastião Bicharra Neto <97459779+SebasNeto@users.noreply.github.com>
2023-06-03 00:00:48 -0300

    Delete elementos da diagonal principal.py

--- elementos da diagonal principal.py
+++
@@ -1,5 +0,0 @@
-matriz = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-for coluna in range(0,5):
-    if coluna == coluna:
-        matriz [coluna][coluna] = int(input())
-print(matriz)
